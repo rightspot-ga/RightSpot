14fea548c7de2bd01e469bb6cb6359717f7c5093
dinssa <manny@dinssa.com>
2023-06-25 18:24:17 +0100

    Add MapIt information about a location when needed

--- location_services/geodetails.py
+++ location_services/geodetails.py
@@ -3,14 +3,16 @@
 
 import requests
 import xmltodict
+import json
 from main_app.static_data.lookups import districts
 
 def geoDetails(lat, lon):
     # Get details about a location using nominatim.org Reverse API.
     # Input must be a latitude and longitude, formatted as floats.
     # Example: (51.508368, -0.274432)
+    # Returns a dictionary of location details, including the district.
 
-    # Build the URL for the API request
+    # Build the URL for the Nominatim API request
     url = 'https://nominatim.openstreetmap.org/reverse'
     params = {
         'lat':lat,
@@ -21,7 +23,23 @@ def geoDetails(lat, lon):
 
     if response.status_code == 200:
         data = xmltodict.parse(response.content)
-        return data['reversegeocode']['addressparts']
+        addressparts = data['reversegeocode']['addressparts']
+        data_processed = addressparts
+        data_processed['district'] = check_uk_district(addressparts)
+    
+    # If the district is not in the addressparts, use mapit to get the district
+    if data_processed['district'] is None:
+        mapit_data = mapit(lat, lon)
+        # Add mapit data to data_processed
+        for key, value in mapit_data.items():
+            data_processed[key] = value
+            for district in districts:
+                if district in value:
+                    data_processed['district'] = district
+                    break
+
+    if response.status_code == 200:
+        return data_processed
 
 def check_uk_district(addressparts):
     if addressparts['country'] != 'United Kingdom':
@@ -35,3 +53,17 @@ def check_uk_district(addressparts):
                 return borough
     return None
 
+def mapit(lat, lon):
+    # Get details about a location using mapit.mysociety.org API.    
+    url = 'https://mapit.mysociety.org/point/4326/' + str(lon) + ',' + str(lat)
+    response = requests.get(url)
+    response_dict = json.loads(response.content)
+
+    data_processed = {}
+
+    # Get all types and names from the response
+    for child in response_dict.values():
+        if isinstance(child, dict) and 'type' in child and 'name' in child:
+            data_processed[child['type']] = child['name']
+            
+    return data_processed
